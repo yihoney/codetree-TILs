fb093eb2a0f7df47305d743b64eeacd2d4c26a82
yi <109622@naver.com>
2023-12-21 12:28:34 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231221/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int[][] graph, visited;
+    static int[][] dirs = new int [][]{ {0,1}, {1,0}};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        visited = new int[n][m];
+        graph = new int[n][m];
+
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        visited[0][0] = 1;
+        dfs(0, 0);   
+
+        System.out.println(visited[n-1][m-1]!=0?1:0);
+    }
+
+    static void dfs(int x, int y) {
+        if(x==n-1 && y==m-1) {
+            return;
+        }
+
+        for(int d=0; d<dirs.length; d++) {
+            int nx = x+dirs[d][0];
+            int ny = y+dirs[d][1];
+
+            if(canGo(nx, ny)) {
+                visited[nx][ny] = visited[x][y]++;
+                dfs(nx, ny);
+            }
+        }
+    }
+
+    static boolean canGo(int x, int y) {
+        if(!inRange(x, y)) { // 그래프 범위를 벗어난다면 
+            return false;
+        }
+
+        if(visited[x][y]==1 || graph[x][y]==0) { // 이미 방문했거나 뱀이 있다면
+            return false;
+        }
+
+        return true;
+    }
+
+    static boolean inRange(int x, int y) {
+        return x>=0 && x<n && y>=0 && y<m;
+    }
+}
